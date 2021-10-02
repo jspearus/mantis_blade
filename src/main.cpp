7852abaefefcbdeab92fa19c04fda2efab77746a
jspearus <jspearus@gmail.com>
2021-10-02 11:14:39 -0500

    added hold posistion mode

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,19 @@ int KD = 0; //0
 
 int I_S_Offset = 4; //5
 
+int setpoint = 0;
+
 //############################ VARIABLES ########################################
 String Data_In = "";
 int POSsen = 0;
+
+int mode = 0;
+bool mode_set = false;
+
+boolean WiiMote = true;
+boolean zButton = false;
+
+bool isOPen = false;
 
 //PID Variables
 double Setpoint, Input, Output;
@@ -44,17 +54,12 @@ Adafruit_Soundboard sfx = Adafruit_Soundboard(&Serial3, NULL, SFX_RST);
 BTS7960 MotorController(M_EN, M_L_PWM, M_R_PWM);
 Nunchuk nchuk;
 
-boolean WiiMote = true;
-
-boolean zButton = false;
-int setpoint = 0;
-
 void setup()
 {
   Serial.begin(115200);  //PC Debug
   Serial3.begin(9600);   ///SoundBoard Port
   Serial4.begin(115200); ///Sensor Glove Port
-  Serial4.setTimeout(100);
+  Serial4.setTimeout(50);
   Serial5.begin(9600); // XBee port
   delay(3000);
   Serial.println("mantis_blade Initializing...");
@@ -91,6 +96,8 @@ void setup()
   PID1.SetSampleTime(10);
   Setpoint = 100;
   Serial5.println(" PID ready..");
+  Serial5.print(" Mode = ");
+  Serial5.println(mode);
   sfx.println("#02");
 }
 
@@ -129,30 +136,91 @@ void loop()
   Input = POSsen;
   setpoint = map(setpoint, 0, 300, 0, 500);
   setpoint = constrain(setpoint, 0, 500);
-  Setpoint = setpoint;
 
-  // Serial.print(Input);
-  // Serial.print(" ");
-  // Serial.println(Setpoint);
-
-  delay(20);
-  if (Input >= Setpoint - I_S_Offset &&
-      Input <= Setpoint + I_S_Offset)
+  if (mode == 1)
   {
-    Output = 0.0;
+    // Dynamic mode
+    // Serial.print(Input);
+    // Serial.print(" ");
+    // Serial.println(Setpoint);
+    Setpoint = setpoint;
+    delay(20);
+    if (Input >= Setpoint - I_S_Offset &&
+        Input <= Setpoint + I_S_Offset)
+    {
+      Output = 0.0;
+    }
+    else
+    {
+      PID1.Compute();
+    }
+    if (mode_set == false && mode > 0)
+    {
+      moveMotor(Output);
+    }
   }
-  else
+
+  else if (mode == 2 && mode_set == false)
   {
+    //hold mode
+    if (setpoint > 250)
+    {
+      isOPen = !isOPen;
+      Serial5.println(isOPen);
+      while (setpoint > 250)
+      {
+        Serial4.print("gfd#");
+        setpoint = map(setpoint, 0, 300, 0, 500);
+        setpoint = constrain(setpoint, 0, 500);
+      }
+    }
+    if (isOPen == false)
+    {
+      Setpoint = 0;
+    }
+
+    else if (isOPen == true)
+    {
+      Setpoint = 500;
+    }
     PID1.Compute();
+    moveMotor(Output);
   }
-  moveMotor(Output);
 }
 
 void serialEvent4()
 { //from Sensor Glove
   // add it to the inputString:
   Data_In = Serial4.readStringUntil('#');
-  setpoint = Data_In.toInt();
+  if (Data_In == "mode")
+  {
+    if (mode < 2)
+    {
+      mode++;
+    }
+    else
+    {
+      mode = 0;
+    }
+    Serial5.print(" mode = ");
+    Serial5.println(mode);
+  }
+  else if (Data_In == "s")
+  {
+    mode_set = true;
+    MotorController.Stop();
+    MotorController.Disable();
+    Serial5.println(" Config...");
+  }
+  else if (Data_In == "a")
+  {
+    mode_set = false;
+    Serial5.println(" armed...");
+  }
+  else if (mode_set == false && mode > 0)
+  {
+    setpoint = Data_In.toInt();
+  }
   Data_In = "";
 }
 
@@ -162,19 +230,19 @@ void moveMotor(int val)
   {
     MotorController.Enable();
     MotorController.TurnLeft(val);
-    //Serial.println(" Out");
+    //Serial5.println(" Out");
   }
   else if (val < -3)
   {
     val = val * -1;
     MotorController.Enable();
     MotorController.TurnRight(val);
-    //Serial.println(" IN");
+    //Serial5.println(" IN");
   }
   else
   {
     MotorController.Stop();
     MotorController.Disable();
-    //Serial.println(" Stop");
+    //Serial5.println(" Stop");
   }
 }
